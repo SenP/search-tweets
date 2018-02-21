7409f1baad9355419222514909103cfff35121fc
Sen P <SenP@users.noreply.github.com>
2018-02-21 11:03:28 +1100

    Update twitterClient.js

--- server/twitterClient.js
+++ server/twitterClient.js
@@ -2,9 +2,8 @@ const Twitter = require('twitter');
 const apiAuthURL = 'https://api.twitter.com/oauth2/token';
 const contentType = 'application/x-www-form-urlencoded;charset=UTF-8';
 const apiSearchURL = 'https://api.twitter.com/1.1/search/tweets.json';
-const consumer_key = 'oH0Ol7jCdqIW4woWWKEVs557H';
-const consumer_secret = 'b3jGiQfiLNeF5Lk8bv979vIBj2gKHvSvjwLobLuxWBMwgEQQri';
-const bearer_token =
-	'AAAAAAAAAAAAAAAAAAAAAFsK2gAAAAAAknDAsE8NzgRecMMb7ohp3O8Bris%3Da9c1ja7suShqEqdELoOrUntvHVVcYMZa3EdbfDmtI4PhqWZSfx';
+const consumer_key = '';
+const consumer_secret = '';
+const bearer_token ='';
 
 module.exports = new Twitter({ consumer_key, consumer_secret, bearer_token });
